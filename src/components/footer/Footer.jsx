a3663c0501155099e9054e3714e8fb5aaab7406d
Manoj <rayimanoj8@gmail.com>
2025-01-30 02:06:11 +0530

    chaged for mobile

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,13 @@
 import logo from "@/components/images/logo.png"
 import {Facebook, Github, Google, Instagram, Mail} from "@icon-park/react";
+import {Separator} from "@/components/ui/separator.jsx";
 export default function Footer(){
-    return <footer className="w-full flex fixed bottom-0 mb-10 mx-10 justify-around">
-        <img src={logo} className="h-16 dark:invert"/>
+    return <footer className="w-full flex fixed bottom-0 mb-10 mx-10 justify-around flex-col lg:flex-row gap-2">
+        <div>
+            <img src={logo} className="h-16 dark:invert"/>
+        </div>
         <div className="space-y-3">
-            <div>
+        <div>
                 <ul className="flex gap-4">
                     <li className="font-semibold">Contact Us</li>
                     <li><Mail className="mx-2 inline-block"/>ms.makes.glory@gmail.com</li>
