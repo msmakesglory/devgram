49dbcd5c8b89d61049103be646b16410f0804052
Manoj <rayimanoj8@gmail.com>
2025-01-29 11:45:15 +0530

    first login form

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,37 @@ import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} f
 import {Label} from "@/components/ui/label.jsx";
 import {Input} from "@/components/ui/input.jsx";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select.jsx";
+import {LoginForm} from "@/components/login-form.jsx";
+import {Switch} from "@/components/ui/switch.jsx";
 
 function App() {
-    return (
-        <Card className="w-[350px] mx-auto my-4">
-            <CardHeader>
-                <CardTitle>Sample Form</CardTitle>
-                <CardDescription>use this template</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="name" placeholder="Name of your project" value='a template'/>
-                        </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="name">Email</Label>
-                            <Select>
-                                <SelectTrigger id="framework">
-                                    <SelectValue placeholder="Select" />
-                                </SelectTrigger>
-                                <SelectContent position="popper">
-                                    <SelectItem value="he">rayimanoj8@gmail.com</SelectItem>
-                                    <SelectItem value="ee">rayimanoj963@gmail.com</SelectItem>
-                                </SelectContent>
-                            </Select>
-                        </div>
-                    </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-                <Button variant="outline">Cancel</Button>
-                <Button>Deploy</Button>
-            </CardFooter>
-        </Card>
-    )
+    const handleLogin = () => {
 
+    }
+    return (
+        <>
+            <Card className='w-[400px] mx-auto mt-3 shadow-lg shadow-white'>
+                <CardHeader className="text-center">
+                    Login
+                </CardHeader>
+                <CardContent className='flex gap-4 flex-col'>
+                    <Label htmlFor='email'>Email</Label>
+                    <Input id='email' placeholder='Enter your username' type='email'/>
+                    <Label htmlFor>Password</Label>
+                    <Input id='password' placeholder='Enter your password' type='password'/>
+                    <div className='flex justify-between'>
+                        <Button className='w-fit' onClick={handleLogin}>
+                            Login
+                        </Button>
+
+                        <Button className='w-fit' onClick={handleLogin}>
+                            Google
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </>
+    )
 }
 
 export default App
