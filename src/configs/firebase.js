01e9a201c565935928a950b87b0e24ccbe53e6e2
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-02-20 14:15:31 +0530

    env set

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -9,13 +9,13 @@ import { initializeFirestore, persistentLocalCache  } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCD0oLKrGS_oPNsLypNRVn7Rq58PwyYTXc",
-  authDomain: "devgram-fc5fc.firebaseapp.com",
-  projectId: "devgram-fc5fc",
-  storageBucket: "devgram-fc5fc.firebasestorage.app",
-  messagingSenderId: "899917472804",
-  appId: "1:899917472804:web:0ff265e2adb8b17b74e4b1",
-  measurementId: "G-6C8MY1XYD0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
