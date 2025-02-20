f9722f01dfe0b265baeaaa1e79e8e32cfa4dccdf
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-02-20 14:05:54 +0530

    offline persistence added

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"
+import { initializeFirestore, persistentLocalCache  } from "firebase/firestore"
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,6 +23,8 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider(app);
 const gitHubProvider = new GithubAuthProvider();
-const db = getFirestore(app);
+const db = initializeFirestore(app, {
+  localCache: persistentLocalCache(),
+})
 
 export {app, auth, googleProvider, db, gitHubProvider}
