53b24c9f70ce4a728fca5a6da9f2a47bf221c288
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-03-01 09:13:46 +0530

    groups data deleted in main

--- src/main.jsx
+++ src/main.jsx
@@ -13,8 +13,6 @@ import NotFound from './components/404.jsx';
 import ForgotPassword from './components/auth/ForgotPassword.jsx';
 import { IdeaProvider } from './context/IdeaContext.jsx';
 import {Home} from "@/components/Home/Home.jsx";
-import CreateGroup from "./components/Group/CreateGroup.jsx";
-import { GroupProvider } from './context/GroupContext.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -22,21 +20,18 @@ createRoot(document.getElementById('root')).render(
             <ProfileProvider> 
                 <AuthProvider>
                     <IdeaProvider>
-                        <GroupProvider>
-                            <Routes>
-                                <Route path="/" element={<App />}>
-                                    <Route path="" element={<LandingPage />} />
-                                    <Route path="/home" element={<Home />} />
-                                    <Route path="/login" element={<Login />} />
-                                    <Route path="/signup" element={<SignUp />} />
-                                    <Route path="/forgotpassword" element={<ForgotPassword />} />
-                                    <Route path="/p/:uid" element={<Profile />} />
-                                    <Route path="/404" element={<NotFound />} />
-                                    <Route path="*" element={<NotFound />} />
-                                    <Route path="/create-group" element={<CreateGroup />} />
-                                </Route>
-                            </Routes>
-                            </GroupProvider>
+                        <Routes>
+                            <Route path="/" element={<App />}>
+                                <Route path="" element={<LandingPage />} />
+                                <Route path="/home" element={<Home />} />
+                                <Route path="/login" element={<Login />} />
+                                <Route path="/signup" element={<SignUp />} />
+                                <Route path="/forgotpassword" element={<ForgotPassword />} />
+                                <Route path="/p/:uid" element={<Profile />} />
+                                <Route path="/404" element={<NotFound />} />
+                                <Route path="*" element={<NotFound />} />
+                            </Route>
+                        </Routes>
                     </IdeaProvider>
                 </AuthProvider>
             </ProfileProvider>
