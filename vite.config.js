562400eff85132cef9dd6c90b036623e2403fc58
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-03-03 19:04:47 +0530

    configuration changed to deploy the app

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/devgram",
+  base: "/", // Hosting at the root path
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Alias for @ -> src/
     },
   },
-})
+});
